eb2066e62de1d57d70a48d6d2c6e82431eb816aa
Matthias Schlaipfer <m.schlaipfer@gmail.com>
2016-04-12 17:59:39 +0200

    Added comments, made more user-friendly.

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # tikz_nametags
-Python script to generate name tags with logos via TikZ
+Python script to generate name tags with logos via TikZ for HERMA 4420
+
+## Run:
+Adjust the parameters in the preamble of mk_nametags.py. If necessary, create
+subdirectories.
+
+python mk_nametags.py
+
+
+## Notes:
+Measurements work with Evince, but seem to be off with Okular.
--- mk_nametags.py
+++ mk_nametags.py
@@ -1,4 +1,24 @@
+IN_FILENAME = "attendees_esc.csv"
+
 NUM_COPIES = 3
+
+LOGO1 = "\includegraphics[trim=20 0 20 90, height=0.5cm]{pics/RiSE_LogoRGB_100323.eps}"
+LOGO2 = "\includegraphics[trim=0 10 0 10, height=0.5cm]{pics/vcla_below_1_CMYK.pdf}"
+LOGO3 = "\includegraphics[trim=275 275 275 275, height=0.5cm]{pics/logics}"
+
+# Adjust to accomodate for input format
+# e.g. change to identity function, if each line contains the wanted tag
+def BEAUTIFY_NAMES(x):
+  a,b = str.split(x.strip(), ",")
+  return b + " " + a
+
+# Adjust to accomodate for input format
+# e.g. do nothing, if each line contains the wanted tag
+def PREPROCESS(f):
+  #get rid of head
+  f.readline()
+
+################################################################################
 
 HEADER = """\documentclass[a4paper,12pt]{article}
 \usepackage{tikz}
@@ -18,20 +38,15 @@ FOOTER = """
 \end{document}
 """
 
-LOGO1 = "\includegraphics[trim=20 0 20 90, height=0.5cm]{pics/RiSE_LogoRGB_100323.eps}"
-LOGO2 = "\includegraphics[trim=0 10 0 10, height=0.5cm]{pics/vcla_below_1_CMYK.pdf}"
-LOGO3 = "\includegraphics[trim=275 275 275 275, height=0.5cm]{pics/logics}"
-
-LABEL_HEIGHT = 3.52
-
 def make_labels(names, start, end):
+  label_height = 3.52
   out = open("tex_files/labels_"+ str(start) + "_" + str(end) + ".tex", 'w')
   out.write(HEADER)
   k = 0
   for i in range(0,len(names),2):
-    top_offset = -k * LABEL_HEIGHT - 0.3
+    top_offset = -k * label_height - 0.3
     name = names[i]
-    out.write("""\\node[anchor=north,minimum height="""+ str(LABEL_HEIGHT) +"""cm, minimum width=9.2cm, text width=7.5cm, align=left] (names) at (4.7cm, """ + str(top_offset) + """cm) {};""")
+    out.write("""\\node[anchor=north,minimum height="""+ str(label_height) +"""cm, minimum width=9.2cm, text width=7.5cm, align=left] (names) at (4.7cm, """ + str(top_offset) + """cm) {};""")
     out.write("\\node[below right=0.5cm] at (names.north west) {" +name+ "};")
     out.write("\\node[above right=0.5cm] (logo1) at (names.south west) {" + LOGO1 + "};")
     out.write("\\node[right=0.9cm] (logo2) at (logo1.east) {" + LOGO2 + "};")
@@ -39,7 +54,7 @@ def make_labels(names, start, end):
 
     if i+1 < len(names):
       name = names[i+1]
-      out.write("""\\node[anchor=north,minimum height="""+ str(LABEL_HEIGHT) +"""cm, minimum width=9.2cm, text width=7.5cm, align=left] (names) at (14.8cm, """ + str(top_offset) + """cm) {};""")
+      out.write("""\\node[anchor=north,minimum height="""+ str(label_height) +"""cm, minimum width=9.2cm, text width=7.5cm, align=left] (names) at (14.8cm, """ + str(top_offset) + """cm) {};""")
       out.write("\\node[below right=0.5cm] at (names.north west) {" +name+ "};")
       out.write("\\node[above right=0.5cm] (logo1) at (names.south west) {" + LOGO1 + "};")
       out.write("\\node[right=0.9cm] (logo2) at (logo1.east) {" + LOGO2 + "};")
@@ -50,16 +65,12 @@ def make_labels(names, start, end):
   out.write(FOOTER)
   out.close()
 
-f = open("attendees_esc.csv")
-#get rid of head
-f.readline()
+f = open(IN_FILENAME)
+PREPROCESS(f)
 
 names = f.readlines()
-def beautify_names(x):
-  a,b = str.split(x.strip(), ",")
-  return b + " " + a
 
-corr_names = map(lambda x: beautify_names(x), names)
+corr_names = map(lambda x: BEAUTIFY_NAMES(x), names)
 all_names = NUM_COPIES*corr_names
 
 for n in range(0,len(all_names), 16):
