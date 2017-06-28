6ea865a507f2637a5b509a49cca0d0661e0c9c04
Matthias Schlaipfer <m.schlaipfer@gmail.com>
2017-06-28 15:33:07 +0200

    sorting, title string

--- mk_nametags.py
+++ mk_nametags.py
@@ -1,4 +1,4 @@
-IN_FILENAME = "attendees_esc.csv"
+IN_FILENAME = "attendees.csv"
 
 NUM_COPIES = 3
 
@@ -10,7 +10,7 @@ LOGO3 = "\includegraphics[trim=275 275 275 275, height=0.5cm]{pics/logics}"
 # e.g. change to identity function, if each line contains the wanted tag
 def BEAUTIFY_NAMES(x):
   a,b = str.split(x.strip(), ",")
-  return b + " " + a
+  return a.title(), b.title()
 
 # Adjust to accomodate for input format
 # e.g. do nothing, if each line contains the wanted tag
@@ -21,6 +21,7 @@ def PREPROCESS(f):
 ################################################################################
 
 HEADER = """\documentclass[a4paper,12pt]{article}
+\usepackage[utf8]{inputenc}
 \usepackage{tikz}
 \usepackage[left=0cm,top=0cm,right=0cm,bottom=0cm,verbose,nohead,nofoot]{geometry}
 
@@ -45,7 +46,7 @@ def make_labels(names, start, end):
   k = 0
   for i in range(0,len(names),2):
     top_offset = -k * label_height - 0.3
-    name = names[i]
+    name = names[i][0] + " " + names[i][1]
     out.write("""\\node[anchor=north,minimum height="""+ str(label_height) +"""cm, minimum width=9.2cm, text width=7.5cm, align=left] (names) at (4.7cm, """ + str(top_offset) + """cm) {};""")
     out.write("\\node[below right=0.5cm] at (names.north west) {" +name+ "};")
     out.write("\\node[above right=0.5cm] (logo1) at (names.south west) {" + LOGO1 + "};")
@@ -53,7 +54,7 @@ def make_labels(names, start, end):
     out.write("\\node[right=0.9cm] (logo3) at (logo2.east) {" + LOGO3 + "};\n\n")
 
     if i+1 < len(names):
-      name = names[i+1]
+      name = names[i+1][0] + " " + names[i+1][1]
       out.write("""\\node[anchor=north,minimum height="""+ str(label_height) +"""cm, minimum width=9.2cm, text width=7.5cm, align=left] (names) at (14.8cm, """ + str(top_offset) + """cm) {};""")
       out.write("\\node[below right=0.5cm] at (names.north west) {" +name+ "};")
       out.write("\\node[above right=0.5cm] (logo1) at (names.south west) {" + LOGO1 + "};")
@@ -70,11 +71,11 @@ PREPROCESS(f)
 
 names = f.readlines()
 
-corr_names = map(lambda x: BEAUTIFY_NAMES(x), names)
+corr_names = sorted(map(lambda x: BEAUTIFY_NAMES(x), names), key=lambda name: name[1])
+
 all_names = NUM_COPIES*corr_names
 
 for n in range(0,len(all_names), 16):
     start = 1*n
     end = 1*n+16
     make_labels(all_names[start:end], start, end)
-
